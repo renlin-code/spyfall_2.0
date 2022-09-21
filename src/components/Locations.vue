90a1a75ff74917d8c4a121cf19a1bc4d6cf8ee1b
renlin-code <renelj1997@gmail.com>
2022-09-21 23:46:19 +0500

    Added modal to locations

--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -1,15 +1,27 @@
 <template>
     <div>
         <h6 class="locations-title">Locations</h6>
+
         <div class="locations-wrapper">
-            <div v-for="item in locations" class="location-card" :key="item.id">
+            <div @click="selectImg(item.url)" v-for="item in locations" class="location-card" :key="item.id">
                 <figure class="location-card__figure">
                     <img :src="item.url" alt="location" class="location-card--image">
                     <div class="location-card__loading-layer"></div>
                 </figure>
                 <p :style="`background-color: ${color}30`" class="location-card--name">{{ item.name.toUpperCase() }}</p>
             </div>
-        </div>    
+        </div>   
+
+        <teleport to="body">
+            <transition name="view">
+                <div @click="showModal = false" v-if="showModal" class="locations-modal">
+                    <!-- <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M24 9.4L22.6 8L16 14.6L9.4 8L8 9.4L14.6 16L8 22.6L9.4 24L16 17.4L22.6 24L24 22.6L17.4 16L24 9.4Z" fill="#f5f5f5"/>
+                    </svg> -->
+                    <img :src="modalSrc" alt="location">
+                </div> 
+            </transition>
+        </teleport>
     </div>
 </template>
 
@@ -18,7 +30,17 @@ import { getColor } from "../mixins/getColor";
 import { fetchLocations } from "../mixins/fetchLocations";
 
 export default {
-    mixins: [fetchLocations(), getColor()]
+    mixins: [fetchLocations(), getColor()],
+    data: () => ({
+        modalSrc: "",
+        showModal: false
+    }),
+    methods: {
+        selectImg(src) {
+            this.modalSrc = src;
+            this.showModal = true;
+        }
+    }
 }
 </script>
 
@@ -45,6 +67,7 @@ export default {
     width: 135px;
     height: auto;
     display: flex;
+    cursor: pointer;
     flex-direction: column;
     justify-content: center;
     align-items: center;
@@ -93,5 +116,25 @@ export default {
     border-radius: 4px;
 }
 
-
+.locations-modal {
+    position: absolute;
+    display: grid;
+    place-content: center;
+    cursor: pointer;
+    left: calc(50% - 205px);
+    top: 0;
+    width: 410px;
+    height: 100vh;
+    background-color: rgba(56, 55, 55, 0.8);
+}
+.locations-modal img {
+    width: 300px;
+    height: auto;
+    border: 4px solid var(--white-color);
+}
+.locations-modal svg {
+    position: absolute;
+    right: 35px;
+    top: 60px;
+}
 </style>
